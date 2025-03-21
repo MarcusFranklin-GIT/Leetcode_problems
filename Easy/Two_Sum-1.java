ae92703ae9c56be2837014165093b57b21ffff04
Marcus Franklin <marcus.franklin.j12@gmail.com>
2025-03-21 22:26:45 +0530

    Create Two_Sum-1.java

---
+++ Easy/Two_Sum-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 1 ; i < nums.length; i++) {
+            for (int y = i; y< nums.length; y++){
+                if (target == nums[y] + nums[y-i])
+                return new int[]{y , y-i};
+            }
+        }
+        return null;
+    }
+}
