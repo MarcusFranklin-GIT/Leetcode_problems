8ca4d649c75e6408d53bbf87aaff7377ce646df5
MarcusFranklin-GIT <marcus.franklin.j12@gmail.com>
2025-07-22 09:11:47 +0530

    palindrome check

---
+++ SkillRack_HOPE/checkpallandromeusingbitmanupulation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class checkpallandromeusingbitmanupulation {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int num=0;
+        for(int i=0;i<str.length();i++){
+            num^=(1<<(str.charAt(i)-'a'));
+
+        }
+        if(num==0|| (num & (num-1))==0){
+            System.out.println("palindrome can be formed when rearranged");
+        } else {
+            System.out.println("NO it is not a palindrome");
+        }
+        sc.close();
+    }
+}
