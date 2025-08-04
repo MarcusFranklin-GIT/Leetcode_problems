c27850da3e792e2b0f8e12f952897791e3f8ce72
MarcusFranklin-GIT <marcus.franklin.j12@gmail.com>
2025-08-04 22:27:02 +0530

    today submission

--- AtCoder/remove.java
+++ AtCoder/remove.java
@@ -1,23 +1,24 @@
 import java.util.*;
 public class remove{
   public static void main(String []args){
-    Scanner sc=new Scanner(System.in);
-    int N=sc.nextInt();
-    int M=sc.nextInt();
-    
-    ArrayList<Integer> nums=new ArrayList<>();
-    for(int i=0;i<N;i++){
-      nums.add(sc.nextInt());
-    }
-    for(int i=0;i<M;i++){
-      int R=sc.nextInt();
-      if(nums.contains(R)){
-        nums.remove(Integer.valueOf(R));
+    try (Scanner sc = new Scanner(System.in)) {
+      int N=sc.nextInt();
+      int M=sc.nextInt();
+      
+      ArrayList<Integer> nums=new ArrayList<>();
+      for(int i=0;i<N;i++){
+        nums.add(sc.nextInt());
       }
-    }
-    
-    while(!nums.isEmpty()){
-      System.out.print(nums.remove(0));
+      for(int i=0;i<M;i++){
+        int R=sc.nextInt();
+        if(nums.contains(R)){
+          nums.remove(Integer.valueOf(R));
+        }
+      }
+      
+      while(!nums.isEmpty()){
+        System.out.print(nums.remove(0));
+      }
     }
   }
 }
