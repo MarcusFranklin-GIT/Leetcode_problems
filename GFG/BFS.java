fed723c1ff8c15bd04cb5e1884afd8092fd1666f
MarcusFranklin-GIT <marcus.franklin.j12@gmail.com>
2025-08-04 21:20:49 +0530

    BFS done

---
+++ GFG/BFS.java
@@ -0,0 +1,27 @@
+class Solution {
+    // Function to return Breadth First Search Traversal of given graph.
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        int S=adj.size();
+        ArrayList<Integer> B=new ArrayList<>();
+        Queue<Integer> Q=new LinkedList<>();
+        boolean visited[]=new boolean[S];
+        
+        Q.add(0);
+        visited[0]=true;
+        
+        while(!Q.isEmpty()){
+            
+            int node =Q.poll();
+            B.add(node);
+            
+            for(int it:adj.get(node)){
+                if(visited[it]==false){
+                    visited[it]=true;
+                    Q.add(it);
+                }
+            }
+        }
+        return B;
+    }
+}
