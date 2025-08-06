b8ef4435e3b4580b4ff996e69da2fb7b0625b2e3
MarcusFranklin-GIT <marcus.franklin.j12@gmail.com>
2025-08-06 19:47:19 +0530

    union find sum 1061

---
+++ Leetcode/Medium/Lexicographically_Smallest_Equivalent_String_1061.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        int leaders[]=new int[26];
+        for(int i=0;i<26;i++){
+            leaders[i]=i;
+        }
+        for(int i=0;i<s1.length();i++){
+            int Left=s1.charAt(i)-'a';
+            int Right=s2.charAt(i)-'a';
+            change(Left,Right,leaders);
+        }
+        for(int i=0;i<26;i++){
+            leaders[i]=find(leaders[i],leaders);
+        }
+        char[] base = baseStr.toCharArray();
+        for(int i=0;i<baseStr.length();i++){
+            base[i]=(char)(leaders[baseStr.charAt(i)-'a']+'a');
+        }
+        String str = String.valueOf(base);
+        return str;
+
+    }
+
+    public static void change(int left,int right,int leaders[]){
+            int Left=find(leaders[left],leaders);
+            int Right=find(leaders[right],leaders);
+            if(Left<Right){
+                leaders[Right]=Left;
+            }else{
+                leaders[Left]=Right;
+            }
+    }
+    public static int find(int node,int []leaders){
+        if(leaders[node]==node)return node;
+        leaders[node]=find(leaders[node],leaders);
+        return leaders[node];
+
+    }
+}
