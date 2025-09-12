70c5488b29b81ec2f8eb3d9de8e9d48b182a1c5b
MarcusFranklin-GIT <marcus.franklin.j12@gmail.com>
2025-09-12 09:11:05 +0530

    leetcode DC done

---
+++ Leetcode/Medium/Vowels_Game_in_a_String_3227.java
@@ -0,0 +1,12 @@
+package Leetcode.Medium;
+
+class Vowels_Game_in_a_String_3227 {
+    public boolean doesAliceWin(String s) {
+     for(int i=0;i<s.length();i++){
+        char c=s.charAt(i);
+        if(c=='a'||c=='o'||c=='i'||c=='e'||c=='u')return true;
+     }
+     return false;   
+    }
+}
+
