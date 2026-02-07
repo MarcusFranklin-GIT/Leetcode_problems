1ae3d65f0add6438c5050c16827f935833379690
MarcusFranklin-GIT <marcus.franklin.j12@gmail.com>
2026-02-07 21:35:16 +0530

    max consecutive ones

---
+++ Leetcode/Medium/Max_Consecutive_Ones III_1004.java
@@ -0,0 +1,23 @@
+package Leetcode.Medium;
+class Max_Consecutive_Ones_1004 {
+    public int longestOnes(int[] nums, int k) {
+        int len=nums.length;
+        int zeros=0;
+        int left=0,right=0;
+        int currlen=0,maxlen=0;
+
+        while(right<len){
+            if(nums[right]==0)zeros++;
+            while(zeros>k){
+                if(nums[left]==0)zeros--;
+                left++;
+            }
+            if(zeros<=k){
+                currlen=right-left+1;
+                maxlen=Math.max(maxlen,currlen);
+            }
+            right++;
+        }
+        return maxlen;
+    }
+}
