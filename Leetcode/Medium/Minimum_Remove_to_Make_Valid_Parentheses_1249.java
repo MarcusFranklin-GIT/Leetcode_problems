e5bcc9879a55dcd098f563fe38fe36897b7812e5
MarcusFranklin-GIT <marcus.franklin.j12@gmail.com>
2026-02-22 23:10:11 +0530

    util package

--- Leetcode/Medium/Minimum_Remove_to_Make_Valid_Parentheses_1249.java
+++ Leetcode/Medium/Minimum_Remove_to_Make_Valid_Parentheses_1249.java
@@ -1,4 +1,5 @@
 package Leetcode.Medium;
+import java.util.Stack;
 class Minimum_Remove_to_Make_Valid_Parentheses_1249 {
     public String minRemoveToMakeValid(String s) {
         Stack<Integer> stk=new Stack<>();
