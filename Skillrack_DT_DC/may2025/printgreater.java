08b217ed011e8f34827ee18db56efbd0b7b961d7
MarcusFranklin <marcus.franklin.j12@gmail.com>
2026-05-16 22:47:48 +0530

    skillrack dc

---
+++ Skillrack_DT_DC/may2025/printgreater.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class printgreater {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc = new Scanner(System.in);
+		int N =sc.nextInt();
+		int M =sc.nextInt();
+		
+		for(int i=0;i<N;i++){
+		    int num=sc.nextInt();
+		    if(num>M){
+		        System.out.print(num+" ");
+		    }
+		}
+
+	}
+}
