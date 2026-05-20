37b4d4264cd58c32626303ba87ac09589e223554
MarcusFranklin <marcus.franklin.j12@gmail.com>
2026-05-20 21:06:16 +0530

    just folder change

---
+++ Leetcode/Easy/Minimum_Common_Value_2540.java
@@ -0,0 +1,13 @@
+public class Minimum_Common_Value_2540 {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int ptr1 =0 ;
+        int ptr2= 0 ;
+        while(ptr1<nums1.length && ptr2<nums2.length){
+            if(nums1[ptr1]==nums2[ptr2])return nums1[ptr1];
+            
+            if(nums1[ptr1]<nums2[ptr2])ptr1++;
+            else if(nums1[ptr1]>nums2[ptr2])ptr2++;
+        }
+        return -1;
+    }
+}
